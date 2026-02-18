722505f85339a698203a026a5768c2082da0d218
maxborshik <maxborshik@icloud.com>
2026-02-18 03:50:15 +0000

    started adding object models

--- backend/content/models.py
+++ backend/content/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    username = models.CharField(max_length=25, unique=True, primary_key=True)
+    first_name = models.CharField(max_length=25)
+    email = models.EmailField(unique=True)
+    subjects = models.ManyToManyField('Subject', through='Enrollment')
+
+class Subject(models.Model):
+    subject_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    icon = models.URLField()
+
+class Enrollment(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    enrollment_date = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    current_grade = models.CharField(max_length=2, blank=True, null=True)
+
+    class Meta:
+        unique_together = ('student', 'subject')
